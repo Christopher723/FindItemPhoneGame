f1b0ef64830a3510b3575a0285e66a7fe3fef105
Christopher <Christopher.woods723@gmail.com>
2023-11-28 18:06:43 -0500

    changed files and added highscore text

--- FindItemPhone/ContentView.swift
+++ FindItemPhone/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
     @State private var timer: Timer?
     @State private var startTime: Date?
     @State var isFinished = false
+    @State var navigateToGameCenter = false
     
     let highScoreKey = "highScore"
     @State private var highScore: Double = UserDefaults.standard.double(forKey: "highScore")
@@ -35,6 +36,12 @@ struct ContentView: View {
         let minutes = Int(elapsedTime) / 60
         return String(format: "%02d:%02d.%03d", minutes, seconds, milliseconds)
     }
+    var formattedScore: String{
+        let milliseconds = Int(highScore * 1000) % 1000
+        let seconds = Int(highScore) % 60
+        let minutes = Int(highScore) / 60
+        return String(format: "%02d:%02d.%03d", minutes, seconds, milliseconds)
+    }
 
     func startTimer() {
         startTime = Date()
@@ -52,128 +59,130 @@ struct ContentView: View {
     
 
     var body: some View {
-        ZStack {
-            Color.black
-                .overlay(
-                    ItemView(isFinished: $isFinished, specialImagePosition: specialImagePosition).environmentObject(myImage)
-                        .mask(
-                            Circle()
-                                .frame(width: 100, height: 100) // Adjust the circle size as needed
-                                .position(self.dragLocation)
-                                .blendMode(.overlay)
-                            
-                        )
-                )
-                .gesture(DragGesture()
-                    .onChanged { value in
-                        self.dragLocation = value.location
-                            .applying(.init(translationX: -45, y: -275)) // Adjust based on the frame size of the image
-                        isMoving = true
-                    }
-                    .onEnded{_ in
-                        isMoving = false
-                    }
-                )
-
-            VStack {
-                Spacer().frame(height: 100)
-                
-                HStack {
-                    Text("Find:")
-                        .font(.title)
-                        .foregroundColor(.white)
-                    
-                    Image(myImage.special)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                        .padding(.bottom, 10)
-                    
-                    Spacer()
-                    
-                    // Display time with milliseconds
-                    Text("Time: \(formattedTime)")
-                        .font(Font.system(.title, design: .monospaced))
-                        .foregroundColor(.white)
-                        .padding(.trailing, 20)
-                }
-                Spacer()
-                
-                ZStack{
-                    HStack {
-                        Spacer()
-                        if isFinished {
-                            Button(action: {
-                                // Reset the game state here
-                                self.dragLocation = .zero
-                                self.isMoving = false
-                                self.elapsedTime = 0.0
-                                self.isFinished = false
-                                self.myImage.reset()
+       
+            ZStack {
+                Color.black
+                    .overlay(
+                        ItemView(isFinished: $isFinished, specialImagePosition: specialImagePosition).environmentObject(myImage)
+                            .mask(
+                                Circle()
+                                    .frame(width: 100, height: 100)
+                                    .position(self.dragLocation)
+                                    .blendMode(.overlay)
                                 
-                                generateRandomPosition()
-                                startTimer()
-                            }) {
-                                Text("Play Again")
-                                    .font(.title)
-                                    .foregroundColor(.white)
-                                    .padding()
-                                    .background(Color.blue)
-                                    .cornerRadius(10)
-                            }
+                            )
+                    )
+                    .gesture(DragGesture()
+                        .onChanged { value in
+                            self.dragLocation = value.location
+                                .applying(.init(translationX: -45, y: -275))
+                            isMoving = true
                         }
+                        .onEnded{_ in
+                            isMoving = false
+                        }
+                    )
+                
+                VStack {
+                    Spacer().frame(height: 100)
+                    
+                    HStack {
+                        Text("Find:")
+                            .font(.title)
+                            .foregroundColor(.white)
+                        
+                        Image(myImage.special)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .padding(.bottom, 10)
                         
                         Spacer()
+                        
+                        // Display time with milliseconds
+                        Text("Time: \(formattedTime)")
+                            .font(Font.system(.title, design: .monospaced))
+                            .foregroundColor(.white)
+                            .padding(.trailing, 20)
                     }
-                    ZStack {
-                        HStack{
-                            Spacer()
-                            Image(systemName: "trophy.fill")
-                                .resizable()
-                                .frame(width: 50, height: 50)
-                                .foregroundStyle(.white)
-                                .padding()
-                                .onTapGesture{
-                                    print(highScore)
-                                
-                                }
-                        }
+                    Spacer()
                     
+                    ZStack{
+                        VStack{
+                            if isFinished {
+                                Text("Highscore: \(formattedScore)")
+                                    .foregroundStyle(.white)
+                                    .font(Font.system(.title, design: .monospaced))
+                            }
+                            HStack {
+                                Spacer()
+                                if isFinished {
+                                    Button(action: {
+                                        
+                                        self.dragLocation = .zero
+                                        self.isMoving = false
+                                        self.elapsedTime = 0.0
+                                        self.isFinished = false
+                                        self.myImage.reset()
+                                        
+                                        generateRandomPosition()
+                                        startTimer()
+                                    }) {
+                                        Text("Play Again")
+                                            .font(.title)
+                                            .foregroundColor(.white)
+                                            .padding()
+                                            .background(Color.blue)
+                                            .cornerRadius(10)
+                                    }
+                                }
+                                
+                                Spacer()
+                            }
+                            
+                            
+                        }.padding()
                     }
-
+                    
+                    
+                }
+            }
+            
+            
+            .edgesIgnoringSafeArea(.all)
+            .onAppear {
+                self.dragLocation = .zero
+                self.isMoving = false
+                self.elapsedTime = 0.0
+                self.isFinished = false
+                self.myImage.reset()
+                
+                generateRandomPosition()
+                startTimer()
+            }
+            
+            .onChange(of: isFinished) {
+                stopTimer()
+                if elapsedTime < highScore && elapsedTime > 0{
+                    highScore = elapsedTime
+                    UserDefaults.standard.set(highScore, forKey: highScoreKey)
+                    
                 }
                 
-                
-                
             }
-        }
-        
-        
-        .edgesIgnoringSafeArea(.all)
-        .onAppear {
-            startTimer()
-        }
-        .onChange(of: isFinished) {
-            stopTimer()
-            if elapsedTime < highScore && elapsedTime > 0{
-            highScore = elapsedTime
-            UserDefaults.standard.set(highScore, forKey: highScoreKey)
-        }
+            .onChange(of: isFinished) {
+                if !isFinished {
+                    startTimer()
+                } else {
+                    stopTimer()
+                }
+            }
             
-        }
-        .onChange(of: isFinished) {
-            if !isFinished {
-                // Restart the timer when isFinished becomes false
-                startTimer()
-            } else {
-                // Stop the timer when isFinished becomes true
-                stopTimer()
-            }
-        }
+            
+            
+        
         
     }
-
-    
 }
 
 
--- FindItemPhone/viewModel.swift
+++ FindItemPhone/viewModel.swift
@@ -29,3 +29,73 @@ class MyViewModel: ObservableObject {
         }
     }
 }
+
+//class GameViewModel{
+//    func authenticateUser() {
+//        GKLocalPlayer.local.authenticateHandler = { vc, error in
+//            guard error == nil else {
+//                print(error?.localizedDescription ?? "")
+//                return
+//            }
+//        }
+//    }
+//    
+//    func loadLeaderboard() async {
+//        
+//        Task{
+//            let leaderboards = try await GKLeaderboard.loadLeaderboards(IDs: ["FruitBoard"])
+//            
+//        }
+//    }
+//    
+//    func leaderboard(elapsedTime: Double) async{
+//        Task{
+//            try await GKLeaderboard.submitScore(
+//                Int(elapsedTime * 1000),
+//                context: 0,
+//                player: GKLocalPlayer.local,
+//                leaderboardIDs: ["FruitBoard"]
+//            )
+//        }
+//    }
+//}
+//public struct GameCenterView: UIViewControllerRepresentable {
+//    let viewController: GKGameCenterViewController
+//    
+//    public init(leaderboardID : String?) {
+//        
+//        if leaderboardID != nil {
+//            self.viewController = GKGameCenterViewController(leaderboardID: leaderboardID!, playerScope: GKLeaderboard.PlayerScope.global, timeScope: GKLeaderboard.TimeScope.allTime)
+//        }
+//        else{
+//            self.viewController = GKGameCenterViewController(state: GKGameCenterViewControllerState.leaderboards)
+//        }
+//        
+//    }
+//    
+//    public func makeUIViewController(context: Context) -> GKGameCenterViewController {
+//        let gkVC = viewController
+//        gkVC.gameCenterDelegate = context.coordinator
+//        return gkVC
+//    }
+//    
+//    public func updateUIViewController(_ uiViewController: GKGameCenterViewController, context: Context) {
+//        return
+//    }
+//    
+//    public func makeCoordinator() -> GKCoordinator {
+//        return GKCoordinator(self)
+//    }
+//}
+//
+//public class GKCoordinator: NSObject, GKGameCenterControllerDelegate {
+//    var view: GameCenterView
+//    
+//    init(_ gkView: GameCenterView) {
+//        self.view = gkView
+//    }
+//    
+//    public func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+//        gameCenterViewController.dismiss(animated: true, completion: nil)
+//    }
+//}
